1992ec4d7ba888af1f589af4f960ec07b7398d96
SHONING <sietske.honing@capgemini.com>
2018-04-25 11:01:18 +0200

    toogling buttons?

--- src/main/resources/static/wardrobe.js
+++ src/main/resources/static/wardrobe.js
@@ -1,17 +1,13 @@
+function getLocation(){
+    $.get("api/location/")
+    //TODO: make the api call, and if we are in narnia we have to toggle the button fight the witch on
+}
+
 function getLog(){
     $.get("/api/log/", function(data){
         if (data.length<=0){
             return;
         }
-
-        html="<p> The status of the wardrobe is </p>";
-        html+="<p> Working: " + data.working + " Open: " + data.open;
-        if(data.amInside == true){
-            html+=" You ARE inside"
-        }else{
-            html+=" You are from the outside looking at the closet!"
-        }
-        html+="</p>"
 
         $("#gameConsole").html(html);
     }
@@ -20,7 +16,6 @@ function getLog(){
 
 function saveWardrobe(e){
     e.preventDefault();
-
     $.post("/api/log/new",
     {
         name: "wardrobe",
@@ -30,9 +25,32 @@ function saveWardrobe(e){
         working: true
     },function(){getLog();}
     );
-
 }
 
 $("#setWardrobe").submit(saveWardrobe);
+
+//If you click the button open, this button wil hide, and Close will show and get in will show
+    $("#open").click(function(){
+        $("#open").hide();
+        $("#close").show();
+        $("#getIn").show();
+    });
+
+
+//If you click close, close will hide, get in will hide and open will show
+    $("#close").click(function(){
+         $("#close").hide();
+         $("#getIn").hide();
+         $("#open").show();
+    });
+
+
+//If you click get in, get in will hide kick will hide and get out will show
+    $("#getIn").click(function(){
+         $("#getIn").hide();
+         $("#getOut").show();
+         $("#kick").hide();
+    }, function(){getLocation();});
+
 
 getLog;

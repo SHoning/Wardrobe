545869734ab13f7583030fa87fd42ccbdd682731
SHONING <sietske.honing@capgemini.com>
2018-04-25 11:42:10 +0200

    Made some small adaptions with fixes in showing/hiding the buttons, and the chance to win against the witch

--- src/main/resources/static/wardrobe.js
+++ src/main/resources/static/wardrobe.js
@@ -1,6 +1,6 @@
 function getLocation(){
     $.get("api/location/", function(data){
-    if (data== true){
+    if (data){
         $("#fight").show();
         $("#gameConsole").html("<h2>you reached Narnia</h2>");
         return;
@@ -11,13 +11,18 @@ function getLocation(){
 
 function fightWitch(){
     $.get("api/location/fight", function(data){
-    if(data==true){
+    if(data){
         $("#fight").hide();
         $("#gameConsole").html("</h2>you have killed the witch<h2><p> you can now talk to Aslan to finish the game</p>");
         $("#talk").show();
         return;
     }
     $("#gameConsole").html("<p>The witch staggers but is still alive, try again!</p>");
+    $("#fight").hide();
+    $("#open").show();
+    $("#kick").show();
+    $("#getOut").hide();
+    $("#close").hide();
     });
 }
 
@@ -54,8 +59,6 @@ function saveWardrobe(e){
 }
 
 $("#setWardrobe").submit(saveWardrobe);
-$("#fight").click(fightWitch());
-$("#talk").click(winGame())
 
 //If you click the start the game button, you can kick and open
 $("#startGame").click(function(){
@@ -81,18 +84,20 @@ $("#startGame").click(function(){
 
 
 //If you click get in, get in will hide kick will hide and get out will show
-    $("#getIn").click(function(){
-         $("#getIn").hide();
-         $("#getOut").show();
-         $("#kick").hide();
-    }, function(){getLocation();});
 
+$("#getIn").click(function() {
+    getLocation();
+    $("#getIn").hide();
+    $("#getOut").show();
+    $("#kick").hide();
+});
 
 $("#getOut").click(function(){
     $("#fight").hide();
     $("#talk").hide();
     $("#getOut").hide();
     $("#getIn").show();
+    $("#kick").show();
 })
 
 getLog;
